74a05ad1ccb389c3b5671ba7f64a11ccec7cccc5
Judah Parsons <GEWDDEWD9@hotmail.com>
2017-08-17 09:54:33 +1200

    Add project files.

---
+++ XamarinHangman/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Graphics;
+using Android.Content;
+
+namespace XamarinHangman
+{
+    [Activity(Label = "XamarinHangman", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.MainMenu);
+            InitializeAllTheThings();
+        }
+
+        private void InitializeAllTheThings()
+        {
+            Android.Content.Res.AssetManager[] manager = new Android.Content.Res.AssetManager[1];   //what on earth is going on here?            
+                Typeface tf = Typeface.CreateFromAsset(manager[0], "spyware_nbp.tff");              //an array shouldn't be necessary, but otherwise this doesn't work
+            
+
+        }
+    }
+}
+
